972fe4f742137cd673cc7b02bdf8dc55f9215b24
Malik Faisal Hakim <malikfaisalhakim@gmail.com>
2022-12-23 19:54:23 +0100

    Update Note.kt

--- app/src/main/java/com/malik/noteapp/model/Note.kt
+++ app/src/main/java/com/malik/noteapp/model/Note.kt
@@ -3,7 +3,6 @@ package com.malik.noteapp.model
 import android.icu.text.CaseMap
 import java.time.LocalDateTime
 import java.util.*
-
 data class Note(
     val id: UUID = UUID.randomUUID(),//Random ID created
     val title: String,
